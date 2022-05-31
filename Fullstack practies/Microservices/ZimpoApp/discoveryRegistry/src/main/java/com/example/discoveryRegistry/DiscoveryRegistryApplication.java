53d26293f1952487c7aad850c7ff59639e4fa45b
Akash kumar <akash@theretailinsights.com>
2022-05-31 12:05:38 +0530

    Initial commit

---
+++ Fullstack practies/Microservices/ZimpoApp/discoveryRegistry/src/main/java/com/example/discoveryRegistry/DiscoveryRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.example.discoveryRegistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryRegistryApplication.class, args);
+	}
+
+}
---
+++ Fullstack practies/Microservices/ZimpoApp/discoveryRegistry/src/test/java/com/example/discoveryRegistry/DiscoveryRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.discoveryRegistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
